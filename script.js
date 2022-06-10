82c368227abd9525fc19b1b302d041df073d0f52
Penguins <o.klekner@gmail.com>
2022-06-10 21:06:33 +0200

    Made the game 1000x smoother, but the bullets are a little off.

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ let degreestwo = 0;
 let game = document.getElementById("game");
 let playerone = document.getElementById("playerone");
 let playertwo = document.getElementById("playertwo");
-var movelength = 5;
+const movelength = 5;
+let moves = []
 
-function moveforward(playerid, degrees){
+function moveforward(playerid, degrees) {
     playerstats = playerid.computedStyleMap();
     let leftm = Math.cos(degrees * Math.PI / 180) * movelength
 
     let topm = Math.sin(degrees * Math.PI / 180) * movelength;
     playerid.style.left = `${playerstats.get("left").value + leftm}px`
     playerid.style.top = `${playerstats.get("top").value + topm}px`
-   
+
 
 }
 
-function shoot(playerid, degrees){
+function shoot(playerid, degrees) {
     playerstats = playerid.computedStyleMap();
 
     let bullet = document.createElement("div")
@@ -27,64 +28,72 @@ function shoot(playerid, degrees){
 
     bullettrajectory.style.top = `${playerstats.get("top").value + 45}px`
     bullettrajectory.style.left = `${playerstats.get("left").value + 50}px`
-   
+
     game.appendChild(bullettrajectory)
     bullettrajectory.appendChild(bullet)
 }
 
 
 window.onkeydown = function (event) {
-    console.log(event.keyCode)
-    //playerone turn right
-    if (event.keyCode == 65) {
-        degreesone += 3
-        document.getElementById("playerone").style.transform = `rotate(${degreesone}deg)`
-    }
-    //playerone turn left
-    if (event.keyCode == 68) {
-        degreesone -= 3
-        document.getElementById("playerone").style.transform = `rotate(${degreesone}deg)`
-    }
-    //player one shoot
+
+    keycod = event.keyCode
+    moves.keys = (moves.keys || []);
+    moves.keys[keycod] = true;
+
     if (event.keyCode == 32) {
-        playeronestats = playerone.computedStyleMap();
-
-        let bullet = document.createElement("div")
-        bullet.className = "bullet"
-        let bullettrajectory = document.createElement("div")
-        bullettrajectory.className = "trajectory"
-        bullettrajectory.style.transform = `rotate(${degreesone}deg)`
-
-        bullettrajectory.style.top = `${playeronestats.get("top").value + 45}px`
-        bullettrajectory.style.left = `${playeronestats.get("left").value + 50}px`
-       
-        game.appendChild(bullettrajectory)
-        bullettrajectory.appendChild(bullet)
+        shoot(playerone, degreesone)
     }
-    if(event.keyCode == 96){
+    if (event.keyCode == 96) {
         shoot(playertwo, degreestwo)
     }
-    //playerone move forward
-    if (event.keyCode == 87) {
-        moveforward(playerone, degreesone)
-    }
-    if(event.keyCode == 38){
-        console.log("move")
-        moveforward(playertwo, degreestwo)
-    }
+}
+
+window.onkeyup = function(e){
+    moves.keys[e.keyCode] = false;
 }
 
 
 
 
 let gameloop = window.setInterval(function () {
+    if(moves.keys && moves.keys[68]){
+        degreesone += 3
+        document.getElementById("playerone").style.transform = `rotate(${degreesone}deg)`
+    }
+    if(moves.keys && moves.keys[65]){
+        degreesone -= 3
+        document.getElementById("playerone").style.transform = `rotate(${degreesone}deg)`
+    }
+    if(moves.keys && moves.keys[39]){
+        degreestwo += 3
+        document.getElementById("playertwo").style.transform = `rotate(${degreestwo}deg)`
+    }
+    if(moves.keys && moves.keys[37]){
+    degreestwo -=3
+        document.getElementById("playertwo").style.transform = `rotate(${degreestwo}deg)`
+    }
+    if(moves.keys && moves.keys[87]){
+        moveforward(playerone, degreesone)
+    }
+    if (moves.keys && moves.keys[38]) {
+        moveforward(playertwo, degreestwo)
+    }
+    if (moves.keys && moves.keys[32]) {
+        shoot(playerone, degreesone)
+    }
+    if (moves.keys && moves.keys[96]) {
+        shoot(playertwo, degreestwo)
+    }
+
+    
     document.querySelectorAll(".bullet").forEach(bullet => {
+        
         let bulletleft = bullet.computedStyleMap().get("left").value;
         let tanktwodata = playertwo.getBoundingClientRect();
 
         bullet.style.left = `${bulletleft + 10}px`
         let rect = bullet.getBoundingClientRect();
-        
+
         if (rect.top >= 0 &&
             rect.left >= 0 &&
             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
@@ -97,12 +106,11 @@ let gameloop = window.setInterval(function () {
         if (rect.left < tanktwodata.left + 100 &&
             rect.left + 10 > tanktwodata.left &&
             rect.top < tanktwodata.top + 100 &&
-            10 + rect.top > tanktwodata.top){
-                console.log("collision")
-            }
+            10 + rect.top > tanktwodata.top) {
+            console.log("collision")
+        }
     }
     )
-
 }
     , 20)
 
